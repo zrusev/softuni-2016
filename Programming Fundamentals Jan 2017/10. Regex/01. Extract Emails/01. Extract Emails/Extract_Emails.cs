6d321c0664ada68134661882739105ae6f70c138
Zlatko Rusev <zlatko.rusev@gmail.com>
2017-02-25 16:58:10 +0200

    10. Regex

---
+++ Programming Fundamentals Jan 2017/10. Regex/01. Extract Emails/01. Extract Emails/Extract_Emails.cs
@@ -0,0 +1,39 @@
+﻿namespace _01.Extract_Emails
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+
+    class Extract_Emails
+    {
+        static void Main()
+        {
+
+            string text = Console.ReadLine();
+            string pattern = @"\b(?<!\S)(([a-z0-9\-\.]+@[a-z0-9\-]+\.[a-z0-9]+([\.a-z0-9]+)?))\b";
+            Regex regex = new Regex(pattern);
+
+            var matches = regex.Matches(text);
+
+            foreach (Match match in matches)
+            {
+                string matchString = match.ToString();
+
+                if (
+                    !(matchString.StartsWith("-")
+                    || matchString.StartsWith("_")
+                    || matchString.StartsWith(".")
+                    || matchString.EndsWith("-")
+                    || matchString.EndsWith("_")
+                    || matchString.EndsWith("."))
+                    )
+                {
+                    Console.WriteLine(match.Value);
+                }
+            }
+
+
+        }
+    }
+}
---
+++ Programming Fundamentals Jan 2017/10. Regex/02. Extract sentences by keyword/02. Extract sentences by keyword/Extract_sentences_by_keyword.cs
@@ -0,0 +1,30 @@
+﻿namespace _02.Extract_sentences_by_keyword
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+
+    class Extract_sentences_by_keyword
+    {
+        static void Main()
+        {
+
+            string word = Console.ReadLine();
+
+            string[] sentences = Console.ReadLine().Split(new[] { '.', '?', '!' }, StringSplitOptions.RemoveEmptyEntries);
+
+            string pattern = "\\b" + word + "\\b";
+            Regex regex = new Regex(pattern);
+
+            foreach (var sentence in sentences)
+            {
+                if (regex.IsMatch(sentence))
+                {
+                    Console.WriteLine(sentence.Trim());
+                }
+            }
+
+        }
+    }
+}
