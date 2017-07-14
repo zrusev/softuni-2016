96d800f7e561d2d0d3b24f1c92f26d52f8cced02
Zlatko Rusev <zlatko.rusev@gmail.com>
2017-07-15 01:28:43 +0300

    05. Exam Preparation

---
+++ 05. C# OOP Basics - June 2017/05. Exam Preparation/CSharp OOP Basics Exam Preparation I/NeedForSpeed/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        var engine = new Engine();
+        engine.Run();
+    }
+}
