6b9fe4489496e69d4ea73f018bcdd2f4ec8ec39c
Zlatko Rusev <zlatko.rusev@gmail.com>
2017-07-14 16:29:16 +0300

    04. Polymorphism

---
+++ 05. C# OOP Basics - June 2017/04. Polymorphism/04. Polymorphism - Lab/04. Lab Polymorphism/03 3. Shapes/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        Circle circle = new Circle(5);
+
+        Console.WriteLine(circle.CalculatePerimeter());
+        Console.WriteLine(circle.CalculateArea());
+    }
+}
