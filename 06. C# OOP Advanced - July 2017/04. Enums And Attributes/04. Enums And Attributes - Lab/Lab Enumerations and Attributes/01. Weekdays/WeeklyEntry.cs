e0c76d8205500e7821888d1efb1b69f95fafacdb
Zlatko Rusev <zlatko.rusev@gmail.com>
2017-08-02 23:10:41 +0300

    04. Enums And Attributes

---
+++ 06. C# OOP Advanced - July 2017/04. Enums And Attributes/04. Enums And Attributes - Lab/Lab Enumerations and Attributes/01. Weekdays/WeeklyEntry.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+public class WeeklyEntry :  IComparable<WeeklyEntry>
+{
+    public WeekDay WeekDay { get; private set; }
+    public string Notes { get; private set; }
+
+    public WeeklyEntry(string weekday, string notes)
+    {
+        this.WeekDay = (WeekDay)Enum.Parse(typeof(WeekDay), weekday);
+        this.Notes = notes;
+    }
+
+    public int CompareTo(WeeklyEntry other)
+    {
+        if (ReferenceEquals(this, other))
+        {
+            return 0;
+        }
+        if (ReferenceEquals(null, other))
+        {
+            return 1;
+        }
+        var weekDayComparison = this.WeekDay.CompareTo(other.WeekDay);
+        if (weekDayComparison != 0)
+        {
+            return weekDayComparison;
+        }
+        return string.Compare(this.Notes, other.Notes, StringComparison.Ordinal);
+    }
+
+    public override string ToString()
+    {
+        return $"{this.WeekDay} - {this.Notes}";
+    }
+}
---
+++ 06. C# OOP Advanced - July 2017/04. Enums And Attributes/04. Enums And Attributes - Lab/Lab Enumerations and Attributes/02. Coffee Machine/CoffeeMachine.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class CoffeeMachine
+{
+    public IEnumerable<CoffeeType> CoffeesSold
+    {
+        get
+        {
+            return this.coffeeSold;
+        }
+    }
+    private int coins;
+    private IList<CoffeeType> coffeeSold;
+
+    public CoffeeMachine()
+    {
+        this.coffeeSold = new List<CoffeeType>();
+    }
+
+    public void BuyCoffee(string size, string type)
+    {
+        CoffeePrice coffeePrice = (CoffeePrice) Enum.Parse(typeof(CoffeePrice), size);
+        CoffeeType coffeeType = (CoffeeType) Enum.Parse(typeof(CoffeeType), type);
+
+        if (this.coins >= (int)coffeePrice)
+        {
+            this.coffeeSold.Add(coffeeType);
+            this.coins = 0;
+        }
+    }
+
+    public void InsertCoin(string coin)
+    {
+        Coin rem = (Coin)Enum.Parse(typeof(Coin), coin);
+        this.coins += (int) rem;
+    }
+}
