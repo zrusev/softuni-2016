96d4741e444105ef06d54f010e7b3e0b3359c78c
Zlatko Rusev <zlatko.rusev@gmail.com>
2017-08-06 19:35:36 +0300

    05. Reflection - Lab

---
+++ 06. C# OOP Advanced - July 2017/05. Reflection/05. Reflection - Lab/Lab Reflection/01. Stealer/Spy .cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+public class Spy
+{
+    public string ClassName { get; set; }
+    public string [] Fields { get; set; }
+    public string StealFieldInfo(string className, params string [] fields)
+    {
+        Type classType = Type.GetType(className);
+        FieldInfo[] classFields = classType.GetFields(BindingFlags.Instance | BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.Public);
+        StringBuilder sb = new StringBuilder();
+
+        Object classsInstance = Activator.CreateInstance(classType, new object[] { });
+
+        sb.AppendLine($"Class under investigation: {className}");
+
+        foreach (FieldInfo field in classFields.Where(f => fields.Contains(f.Name)))
+        {
+            sb.AppendLine($"{field.Name} = {field.GetValue(classsInstance)}");
+        }
+
+        return sb.ToString().Trim();
+    }
+}
