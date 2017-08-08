606d234d995745ccda81841b7ca56de42af5ea23
Zlatko Rusev <zlatko.rusev@gmail.com>
2017-08-08 21:18:14 +0300

    05. BarrackWars - Return

---
+++ 06. C# OOP Advanced - July 2017/05. Reflection/05. Reflection - Exercise/Exercises Reflection/05. BarrackWars - Return/Core/CommandInterpreter.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.CodeDom;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using _03BarracksFactory.Attributes;
+using _03BarracksFactory.Contracts;
+
+namespace _03BarracksFactory.Core
+{
+    public class CommandInterpreter : ICommandInterpreter
+    {
+        private const string CommandSuffix = "Command";
+
+        private IRepository repository;
+        private IUnitFactory uniteFactory;
+
+        public CommandInterpreter(IRepository repository, IUnitFactory uniteFactory)
+        {
+            this.repository = repository;
+            this.uniteFactory = uniteFactory;
+        }
+
+        public IExecutable InterpretCommand(string[] data, string commandName)
+        {
+            string commandCompleteName = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(commandName) + CommandSuffix;
+
+            Type commandType = Assembly.GetEntryAssembly().GetTypes().FirstOrDefault(t => t.Name == commandCompleteName);
+
+            object[] commandParrams =
+            {
+                data
+            };
+
+            if (commandType == null)
+            {
+                throw new InvalidOperationException("Invalid command!");
+            }
+
+            IExecutable currentCommand = (IExecutable)Activator.CreateInstance(commandType, commandParrams);
+
+            currentCommand = this.InjectDependencies(currentCommand);
+
+            return currentCommand;
+        }
+
+        private IExecutable InjectDependencies(IExecutable currentCommand)
+        {
+            FieldInfo[] commandFields = currentCommand.GetType().GetFields(BindingFlags.Instance | BindingFlags.NonPublic)
+                .Where(f => f.GetCustomAttributes<InjectAttribute>() != null).ToArray();
+
+            FieldInfo[] interpreterFields = this.GetType().GetFields(BindingFlags.Instance | BindingFlags.NonPublic);
+
+            foreach (var field in commandFields)
+            {
+                field.SetValue(currentCommand, interpreterFields.First(f => f.FieldType == field.FieldType).GetValue(this));
+            }
+
+            return currentCommand;
+        }
+    }
+}
