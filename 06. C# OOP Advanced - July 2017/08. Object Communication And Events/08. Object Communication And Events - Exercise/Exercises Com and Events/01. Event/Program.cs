eba21f4cffcd30def34147c70359b18919ba1153
Zlatko Rusev <zlatko.rusev@gmail.com>
2017-08-19 13:17:25 +0300

    08. Object Communication And Events

---
+++ 06. C# OOP Advanced - July 2017/08. Object Communication And Events/08. Object Communication And Events - Exercise/Exercises Com and Events/01. Event/Program.cs
@@ -0,0 +1,68 @@
+﻿namespace _01.Event
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            Dispatcher dispacher = new Dispatcher();
+            Handler handler = new Handler();
+
+            dispacher.NameChange += handler.OnDispatcherNameChange;
+
+            string name = Console.ReadLine();
+
+            while (name != "End")
+            {
+                dispacher.Name = name;
+                name = Console.ReadLine();
+            }
+        }
+    }
+
+    public class NameChangeEventArgs
+    {
+        public NameChangeEventArgs(string name)
+        {
+            this.Name = name;
+        }
+
+        public string Name { get; set; }
+    }
+
+    public class Handler
+    {
+        public void OnDispatcherNameChange(object sender, NameChangeEventArgs args)
+        {
+            Console.WriteLine($"Dispatcher's name changed to {args.Name}.");
+        }
+    }
+
+    public delegate void NameChangeEventHandler(object sender, NameChangeEventArgs e);
+
+    public class Dispatcher
+    {
+        private string name;
+
+        public event NameChangeEventHandler NameChange;
+
+        public string Name
+        {
+            get { return this.name; }
+            set
+            {
+                this.name = value;
+                OnNameChange(new NameChangeEventArgs(value));
+            }
+        }
+
+        private void OnNameChange(NameChangeEventArgs e)
+        {
+            if (NameChange != null)
+            {
+                NameChange(this, e);
+            }
+        }
+    }
+}
---
+++ 06. C# OOP Advanced - July 2017/08. Object Communication And Events/08. Object Communication And Events - Exercise/Exercises Com and Events/02. Kings/Program.cs
@@ -0,0 +1,106 @@
+﻿namespace _02.Kings
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            List<Soldier> army = new List<Soldier>();
+            King king = new King(Console.ReadLine());
+
+            string[] royalGuards = Console.ReadLine().Split();
+            foreach (var royalGuard in royalGuards)
+            {
+                RoyalGuard guard = new RoyalGuard(royalGuard);
+                army.Add(guard);
+                king.UnderAttack += guard.KingUnderAttack;
+            }
+
+            string[] footmen = Console.ReadLine().Split();
+            foreach (var footman in footmen)
+            {
+                Footman foot = new Footman(footman);
+                army.Add(foot);
+                king.UnderAttack += foot.KingUnderAttack;
+            }
+
+            string[] command = Console.ReadLine().Split();
+            while (command[0] != "End")
+            {
+                switch (command[0])
+                {
+                    case "Kill":
+                        Soldier soldier = army.FirstOrDefault(s => s.Name == command[1]);
+                        king.UnderAttack -= soldier.KingUnderAttack;
+                        army.Remove(soldier);
+                        break;
+                    case "Attack":
+                        king.OnUnderAttack();
+                        break;
+                }
+                command = Console.ReadLine().Split();
+            }
+        }
+    }
+
+    public abstract class Soldier
+    {
+        public Soldier(string name)
+        {
+            this.Name = name;
+        }
+
+        public string Name { get; private set; }
+
+        public abstract void KingUnderAttack(object sender, EventArgs e);
+    }
+
+    public class RoyalGuard : Soldier
+    {
+        public RoyalGuard(string name) : base(name)
+        {
+        }
+
+        public override void KingUnderAttack(object sender, EventArgs e)
+        {
+            Console.WriteLine($"Royal Guard {this.Name} is defending!");
+        }
+    }
+
+    public class King
+    {
+        public event EventHandler UnderAttack;
+
+        public King(string name)
+        {
+            this.Name = name;
+        }
+
+        public string Name { get; private set; }
+
+        public void OnUnderAttack()
+        {
+            Console.WriteLine($"King {this.Name} is under attack!");
+
+            if (UnderAttack != null)
+            {
+                UnderAttack(this, new EventArgs());
+            }
+        }
+    }
+
+    public class Footman : Soldier
+    {
+        public Footman(string name) : base(name)
+        {
+        }
+
+        public override void KingUnderAttack(object sender, EventArgs e)
+        {
+            Console.WriteLine($"Footman {this.Name} is panicking!");
+        }
+    }
+}
