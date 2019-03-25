40d5416f217cd4fc0b898100368e5071d8f05096
Zlatko Rusev <zlatko.rusev@students.softuni.bg>
2019-03-25 10:55:03 +0200

    04. Code-First

---
+++ 16. Databases Advanced - Entity Framework - Feb 2019/04. Code-First/HospitalDatabase/HospitalDatabase.Data/Config.cs
@@ -0,0 +1,7 @@
+﻿namespace HospitalDatabase.Data
+{
+    public class Config
+    {
+        public const string ConnectionString = @"Data Source=(LocalDB)\LocalDB;Database=HospitalDatabase;Integrated Security=SSPI;";
+    }
+}
---
+++ 16. Databases Advanced - Entity Framework - Feb 2019/04. Code-First/HospitalDatabase/HospitalDatabase/StartUp.cs
@@ -0,0 +1,9 @@
+﻿namespace HospitalDatabase
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+        }
+    }
+}
