52d29c5fa48b216d263dd77ecf56b7236756be02
Zlatko Rusev <zlatko.rusev@students.softuni.bg>
2019-04-11 18:33:55 +0300

    service update

--- 14. Angular Fundamentals - Мar 2019/01. Workshop Movie DB/movie-finder/src/app/movies/movies.component.ts
+++ 14. Angular Fundamentals - Мar 2019/01. Workshop Movie DB/movie-finder/src/app/movies/movies.component.ts
@@ -1,30 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { MoviesService } from '../service/movies.service';
 import Movie from 'src/Models/Movie';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-movies',
   templateUrl: './movies.component.html',
   styleUrls: ['./movies.component.css']
 })
-export class MoviesComponent implements OnInit {
-  popularMovies: Array<Movie>;
-  inTheaterMovies: Array<Movie>;
+export class MoviesComponent implements OnInit, OnDestroy {
+  popularMovies: Movie[];
+  inTheaterMovies: Movie[];
+  popularKidsMovies: Movie[];
+  bestDramaMovies: Movie[];
+
+  popularMoviesSub: Subscription;
+
   message: string = null;
 
   constructor(private moviesService: MoviesService) { }
 
   ngOnInit() {
-    this.moviesService.getPopularMovies().subscribe(data => {
-      this.popularMovies = data['results'].slice(0, 6);
+    this.popularMoviesSub = this.moviesService.getPopularMovies().subscribe(data => {
+      this.popularMovies = data;
     });
 
     this.moviesService.getInTheaterMovies().subscribe(data => {
-      this.inTheaterMovies = data['results'].slice(6, 12);
+      this.inTheaterMovies = data;
+    });
+
+    this.moviesService.getPopularKidsMovies().subscribe(data => {
+      this.popularKidsMovies = data;
+    });
+
+    this.moviesService.getBestDramaMovies().subscribe(data => {
+      this.bestDramaMovies = data;
     });
   }
 
+  ngOnDestroy(): void {
+    this.popularMoviesSub.unsubscribe();
+  }
+
   fromChild(event) {
-    this.message = 'Button with id: '+ event + ' has been clicked!';
+    this.message = 'Button with id: ' + event + ' has been clicked!';
   }
 }
--- 14. Angular Fundamentals - Мar 2019/01. Workshop Movie DB/movie-finder/src/app/service/movies.service.ts
+++ 14. Angular Fundamentals - Мar 2019/01. Workshop Movie DB/movie-finder/src/app/service/movies.service.ts
@@ -2,10 +2,13 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from '../../../node_modules/rxjs';
 import Movie from '../../Models/Movie';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
 
 const BASE_URL = 'https://api.themoviedb.org/3/movie';
 const IN_THEATER = 'https://api.themoviedb.org/3/discover/movie';
-const API_KEY = '?api_key=';
+const API_KEY = '?api_key=0290ab7f95ccdca4514a6a26745d9eb1';
+const KIDS = '&certification_country=US&certification.lte=G&sort_by=popularity.desc';
+const BEST_DRAMA = '&with_genres=18&primary_release_year=2019';
 
 @Injectable({
   providedIn: 'root'
@@ -16,10 +19,30 @@ export class MoviesService {
   constructor(private http: HttpClient) { }
 
   getPopularMovies(): Observable<Array<Movie>> {
-    return this.http.get<Array<Movie>>(BASE_URL + '/popular' + API_KEY);
+    return this.http.get<Movie[]>(BASE_URL + '/popular' + API_KEY)
+      .pipe(
+        map((data) => data['results'].slice(0, 6))
+      );
   }
 
   getInTheaterMovies(): Observable<Array<Movie>> {
-    return this.http.get<Array<Movie>>(IN_THEATER + API_KEY + '&with_release_type=2|3');
+    return this.http.get<Movie[]>(IN_THEATER + API_KEY + '&with_release_type=2|3')
+      .pipe(
+        map((data) => data['results'].slice(0, 6))
+      );
+  }
+
+  getPopularKidsMovies() {
+    return this.http.get<Movie[]>(IN_THEATER + API_KEY + KIDS)
+      .pipe(
+        map((data) => data['results'].slice(0, 6))
+      );
+  }
+
+  getBestDramaMovies() {
+    return this.http.get<Movie[]>(IN_THEATER + API_KEY + BEST_DRAMA)
+    .pipe(
+      map((data) => data['results'].slice(0, 6))
+    );
   }
 }
