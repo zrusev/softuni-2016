473a3ce4ccdf4814a210b1f46c2c8abe4d5d6b4c
Zlatko Rusev <zlatko.rusev@students.softuni.bg>
2019-04-12 14:57:04 +0300

    form.controls

--- 14. Angular Fundamentals - Мar 2019/02. Forms/forms/src/app/app.module.ts
+++ 14. Angular Fundamentals - Мar 2019/02. Forms/forms/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { RegisterFormComponent } from './register-form/register-form.component';
@@ -10,7 +11,8 @@ import { RegisterFormComponent } from './register-form/register-form.component';
     RegisterFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- 14. Angular Fundamentals - Мar 2019/02. Forms/forms/src/app/register-form/register-form.component.ts
+++ 14. Angular Fundamentals - Мar 2019/02. Forms/forms/src/app/register-form/register-form.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register-form.component.css']
 })
 export class RegisterFormComponent implements OnInit {
+  phoneNumbers: string[] = ["+971", "+359", "+972", "+198", "+701"];
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  register(value: any) {
+console.log(value);
+
+  }
 }
