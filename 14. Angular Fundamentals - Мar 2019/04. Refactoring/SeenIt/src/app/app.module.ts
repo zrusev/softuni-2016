ab4dea79470793d9dc1ebf86b42df00847476ca5
Zlatko Rusev <zlatko.rusev@students.softuni.bg>
2019-04-14 23:37:02 +0300

    post module

--- 14. Angular Fundamentals - Мar 2019/04. Refactoring/SeenIt/src/app/app.module.ts
+++ 14. Angular Fundamentals - Мar 2019/04. Refactoring/SeenIt/src/app/app.module.ts
@@ -9,29 +9,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './components/authentication/register/register.component';
 import { LoginComponent } from './components/authentication/login/login.component';
-import { PostListComponent } from './components/post/post-list/post-list.component';
-import { PostCreateComponent } from './components/post/post-create/post-create.component';
-import { PostEditComponent } from './components/post/post-edit/post-edit.component';
-import { PostDetailsComponent } from './components/post/post-details/post-details.component';
-import { PostInfoComponent } from './components/post/post-info/post-info.component';
-import { CommentInfoComponent } from './components/comment/comment-info/comment-info.component';
-import { CommentCreateComponent } from './components/comment/comment-create/comment-create.component';
 import { TokenInterceptor } from './core/interceptors/token.interceptor';
 import { ErrorInterceptor } from './core/interceptors/error.interceptor';
 import { SharedModule } from './components/shared/shared.module';
+import { PostModule } from './components/post/post.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     RegisterComponent,
-    LoginComponent,
-    PostListComponent,
-    PostCreateComponent,
-    PostEditComponent,
-    PostDetailsComponent,
-    PostInfoComponent,
-    CommentInfoComponent,
-    CommentCreateComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
@@ -40,7 +27,8 @@ import { SharedModule } from './components/shared/shared.module';
     HttpClientModule,
     BrowserAnimationsModule,
     ToastrModule.forRoot(),
-    SharedModule
+    SharedModule,
+    PostModule
   ],
   providers: [{
     provide: HTTP_INTERCEPTORS,
---
+++ 14. Angular Fundamentals - Мar 2019/04. Refactoring/SeenIt/src/app/components/post/post.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { PostCreateComponent } from './post-create/post-create.component';
+import { PostDetailsComponent } from './post-details/post-details.component';
+import { PostEditComponent } from './post-edit/post-edit.component';
+import { PostInfoComponent } from './post-info/post-info.component';
+import { PostListComponent } from './post-list/post-list.component';
+import { CommentInfoComponent } from '../comment/comment-info/comment-info.component';
+import { CommentCreateComponent } from '../comment/comment-create/comment-create.component';
+
+@NgModule({
+    declarations: [
+        PostCreateComponent,
+        PostDetailsComponent,
+        PostEditComponent,
+        PostInfoComponent,
+        PostListComponent,
+        CommentInfoComponent,
+        CommentCreateComponent
+    ],
+    imports: [
+        CommonModule,
+        FormsModule,
+        RouterModule
+    ],
+    exports: []
+})
+export class PostModule {
+
+}
