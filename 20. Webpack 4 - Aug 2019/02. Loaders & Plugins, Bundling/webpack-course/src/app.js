1d4c01c6a8587438d496ea598b3e7d594f5e349a
Zlatko Rusev <zlatko.rusev@students.softuni.bg>
2019-08-29 09:48:45 +0300

    Webpack 4

---
+++ 20. Webpack 4 - Aug 2019/02. Loaders & Plugins, Bundling/webpack-course/src/app.js
@@ -0,0 +1,24 @@
+import './motorcycle.jpeg';
+import './apple.png';
+import './main.css';
+import 'core-js';
+//import MyLibrary from './my-library.js';
+
+// MyLibrary();
+
+// class Foo {
+//     constructor(name) {
+//         this.name = name;
+//     }
+// }
+
+// Promise.resolve(10);
+
+//console.log(new Foo('pesho').foo());
+
+const foo = {
+    a: "one",
+    b: "two"
+}
+
+console.log({...foo, c: "three"});
