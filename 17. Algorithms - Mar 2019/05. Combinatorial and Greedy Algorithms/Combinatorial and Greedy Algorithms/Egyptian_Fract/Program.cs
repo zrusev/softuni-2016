db8f940953bb6cdedf982b33ccb1924d352d4f3c
Zlatko Rusev <zlatko.rusev@gmail.com>
2021-04-05 21:41:46 +0300

    05. Combinatorial and Greedy Algorithms

---
+++ 17. Algorithms - Mar 2019/05. Combinatorial and Greedy Algorithms/Combinatorial and Greedy Algorithms/Egyptian_Fract/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace Egyptian_Fract
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var number = Console.ReadLine().Split('/');
+
+            var numerator = long.Parse(number[0]); // 43
+            var denominator = long.Parse(number[1]); // 48
+
+            if (denominator < numerator)
+            {
+                Console.WriteLine("Error (fraction is equal to or greater than 1)");
+                return;
+            }
+
+            Console.Write($"{numerator}/{denominator} = ");
+
+            var index = 2;
+            var result = new List<int>();
+
+            while (numerator != 0)
+            {
+                // 1 / 2
+                var nextNumerator = numerator * index; // 43 * 2
+                var indexNumerator = denominator; // 48 * 1
+
+                var remaining = nextNumerator - indexNumerator; // 86 - 48
+
+                if (remaining < 0)
+                {
+                    index++;
+                    continue;
+                }
+
+                result.Add(index);
+
+                numerator = remaining;
+                denominator = denominator * index;
+
+                index++;
+            }
+
+            Console.WriteLine(string.Join(" + ", result.Select(r => $"1/{r}")));
+        }
+    }
+}
---
+++ 17. Algorithms - Mar 2019/05. Combinatorial and Greedy Algorithms/Combinatorial and Greedy Algorithms/Knapsack/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace Knapsack
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Program
+    {
+        internal class Item
+        {
+            public double Weight { get; set; }
+
+            public double Price { get; set; }
+        }
+
+        public static void Main()
+        {
+            var capacity = double.Parse(Console.ReadLine().Split(' ')[1]);
+            var items = double.Parse(Console.ReadLine().Split(' ')[1]);
+
+            var allItems = new List<Item>();
+
+            for (int i = 0; i < items; i++)
+            {
+                var currentItem = Console.ReadLine().Split(" -> ");
+
+                allItems.Add(new Item
+                {
+                    Price = double.Parse(currentItem[0]),
+                    Weight = double.Parse(currentItem[1])
+                });
+            }
+
+            allItems = allItems
+                .OrderByDescending(i => i.Price / i.Weight)
+                .ToList();
+
+            var index = 0;
+            var totalPrice = 0.0;
+
+            while(capacity > 0 && index < allItems.Count)
+            {
+                var currentItem = allItems[index];
+                var takenQuantity = Math.Min(capacity, currentItem.Weight);
+
+                var percentageQuantity = takenQuantity / currentItem.Weight;
+
+                totalPrice += percentageQuantity * currentItem.Price;
+                capacity -= takenQuantity;
+                
+                index++;
+
+                var percQuantity = percentageQuantity == 1 ? "100" : $"{percentageQuantity * 100:F2}";
+
+                Console.WriteLine($"Take {percQuantity}% of item with price {currentItem.Price:F2} and weight {currentItem.Weight:F2}");
+            }
+
+            Console.WriteLine($"Total price: {totalPrice:F2}");
+        }
+    }
+}
