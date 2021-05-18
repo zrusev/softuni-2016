d201ac87403bad65e847b3ec5e96d783f8566987
Zlatko Rusev <zlatko.rusev@gmail.com>
2021-05-18 22:29:52 +0300

    C# Masterclass Exam - 16 May 2021

--- 27. C# Masterclass - Mar 2021/C# Masterclass Exam - 16 May 2021/DependencyContainer/MasterInjection/MyServiceProvider.cs
+++ 27. C# Masterclass - Mar 2021/C# Masterclass Exam - 16 May 2021/DependencyContainer/MasterInjection/MyServiceProvider.cs
@@ -43,6 +43,11 @@
                 return mappings[type];
             }
 
+            if (!mappings.Values.Contains(type))
+            {
+                return default(Type);
+            }
+
             return type;
         }
 
