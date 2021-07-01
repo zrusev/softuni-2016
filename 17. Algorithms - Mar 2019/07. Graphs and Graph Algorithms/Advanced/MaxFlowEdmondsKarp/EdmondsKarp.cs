6f6dbf7c1474527dcde547dcfc4b2d109947d729
Zlatko Rusev <zlatko.rusev@gmail.com>
2021-07-01 14:10:49 +0300

    Advanced Graphs

---
+++ 17. Algorithms - Mar 2019/07. Graphs and Graph Algorithms/Advanced/MaxFlowEdmondsKarp/EdmondsKarp.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class EdmondsKarp
+{
+    private static int[][] graph;
+    private static int[] parent;
+
+    public static int FindMaxFlow(int[][] targetGraph)
+    {
+        graph = targetGraph;
+
+        parent = Enumerable.Repeat(-1, graph.Length).ToArray();
+
+        var maxFlow = 0;
+        var start = 0;
+        var end = graph.Length - 1;
+
+        while (Bfs(start, end))
+        {
+            var pathFlow = int.MaxValue;
+            var currentNode = end;
+
+            while (currentNode != start)
+            {
+                var prevNode = parent[currentNode];
+                var currentFlow = graph[prevNode][currentNode];
+
+                if (currentFlow > 0 && currentFlow < pathFlow)
+                {
+                    pathFlow = currentFlow;
+                }
+
+                currentNode = prevNode;
+            }
+
+            maxFlow += pathFlow;
+
+            currentNode = end;
+
+            while (currentNode != start)
+            {
+                var prevNode = parent[currentNode];
+
+                graph[prevNode][currentNode] -= pathFlow;
+                graph[currentNode][prevNode] += pathFlow;
+
+                currentNode = prevNode;
+            }
+        }
+
+        return maxFlow;
+    }
+
+    private static bool Bfs(int start, int end)
+    {
+        var visited = new bool[graph.Length];
+
+        Queue<int> queue = new Queue<int>();
+        queue.Enqueue(start);
+        visited[start] = true;
+
+        while (queue.Count > 0)
+        {
+            var node = queue.Dequeue();
+            for (int child = 0; child < graph[node].Length; child++)
+            {
+                if (graph[node][child] > 0 && !visited[child])
+                {
+                    queue.Enqueue(child);
+                    parent[child] = node;
+                    visited[child] = true;
+                }
+            }
+        }
+
+        return visited[end];
+    }
+}
---
+++ 17. Algorithms - Mar 2019/07. Graphs and Graph Algorithms/Advanced/StronglyConnectedComponents/StronglyConnectedComponents.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class StronglyConnectedComponents
+{
+    private static List<int>[] graph;
+    private static List<int>[] reversedGraph;
+    private static bool[] visited;
+
+    private static Stack<int> stack = new Stack<int>();
+
+    private static List<List<int>> stronglyConnectedComponents;
+
+    public static List<List<int>> FindStronglyConnectedComponents(List<int>[] targetGraph)
+    {
+        graph = targetGraph;
+        visited = new bool[graph.Length];
+
+        BuildReversedGraph();
+
+        for (int node = 0; node < graph.Length; node++)
+        {
+            if (!visited[node])
+            {
+                Dfs(node);
+            }
+        }
+
+        stronglyConnectedComponents = new List<List<int>>();
+
+        visited = new bool[graph.Length];
+
+        while (stack.Count > 0)
+        {
+            var node = stack.Pop();
+
+            if (!visited[node])
+            {
+                stronglyConnectedComponents.Add(new List<int>());
+                ReverseDfs(node);
+            }
+        }
+
+        return stronglyConnectedComponents;
+    }
+
+    private static void BuildReversedGraph()
+    {
+        reversedGraph = new List<int>[graph.Length];
+
+        for (int node = 0; node < reversedGraph.Length; node++)
+        {
+            reversedGraph[node] = new List<int>();
+        }
+
+        for (int node = 0; node < graph.Length; node++)
+        {
+            foreach (var child in graph[node])
+            {
+                reversedGraph[child].Add(node);
+            }
+        }
+    }
+
+    private static void ReverseDfs(int node)
+    {
+        if (!visited[node])
+        {
+            visited[node] = true;
+
+            stronglyConnectedComponents.Last().Add(node);
+
+            foreach (var child in reversedGraph[node])
+            {
+                ReverseDfs(child);
+            }
+        }
+    }
+
+    private static void Dfs(int node)
+    {
+        if (!visited[node])
+        {
+            visited[node] = true;
+
+            foreach (var child in graph[node])
+            {
+                Dfs(child);
+            }
+
+            stack.Push(node);
+        }
+    }
+}
