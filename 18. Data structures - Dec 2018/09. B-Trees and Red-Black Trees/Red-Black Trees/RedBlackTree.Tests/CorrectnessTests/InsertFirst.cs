85e4807755e6242fd3f136daee0e193055d1cc16
Zlatko Rusev <zlatko.rusev@gmail.com>
2021-10-06 18:37:11 +0300

    B-Trees and Red-Black Trees

---
+++ 18. Data structures - Dec 2018/09. B-Trees and Red-Black Trees/Red-Black Trees/RedBlackTree.Tests/CorrectnessTests/InsertFirst.cs
@@ -0,0 +1,17 @@
+﻿namespace RedBlackTree.Tests.CorrectnessTests
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    class InsertFirst
+    {
+        [Test]
+        public void Insert_SingleElement_ShouldIncreaseCount()
+        {
+            RedBlackTree<int> rbt = new RedBlackTree<int>();
+            rbt.Insert(5);
+
+            Assert.AreEqual(1, rbt.Count());
+        }
+    }
+}
