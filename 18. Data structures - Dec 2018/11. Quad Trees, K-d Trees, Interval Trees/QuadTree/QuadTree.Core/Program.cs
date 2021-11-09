5ec5f42fd059387ff10e1534682f6088cd71952a
Zlatko Rusev <zlatko.rusev@gmail.com>
2021-11-09 21:57:03 +0200

    QuadTree

---
+++ 18. Data structures - Dec 2018/11. Quad Trees, K-d Trees, Interval Trees/QuadTree/QuadTree.Core/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace QuadTree.Core
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            QuadTree<TestBox> quadTree = new QuadTree<TestBox>(200, 200, 5);
+            var source = new TestBox(0, 0);
+
+            quadTree.Insert(source);
+            quadTree.Insert(new TestBox(0, 10));
+            quadTree.Insert(new TestBox(0, 10));
+            quadTree.Insert(new TestBox(0, 10));
+            quadTree.Insert(new TestBox(0, 10));
+
+            var collisions = quadTree.Report(source.Bounds);
+        }
+    }
+}
