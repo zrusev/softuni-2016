a3635051267ce5705fe254480e38d4b62f66da34
Zlatko Rusev <zlatko.rusev@gmail.com>
2021-11-15 15:15:43 +0200

    Shopping Center

---
+++ 18. Data structures - Dec 2018/13. Combining Data Structures/Shopping Center/Shopping Center/ShoppingCenter.cs
@@ -0,0 +1,110 @@
+﻿using Wintellect.PowerCollections;
+
+namespace Shopping_Center
+{
+    public class ShoppingCenter
+    {
+        private Dictionary<string, Bag<Product>> byProducer;
+        private Dictionary<string, Bag<Product>> byName;
+        private OrderedBag<Product> byPrice;
+
+        public ShoppingCenter()
+        {
+            this.byProducer = new Dictionary<string, Bag<Product>>();
+            this.byName = new Dictionary<string, Bag<Product>>();
+            this.byPrice = new OrderedBag<Product>((x, y) => x.Price.CompareTo(y.Price));
+        }
+
+        public void Add(Product product)
+        {
+            if (!this.byProducer.ContainsKey(product.Producer))
+            {
+                this.byProducer[product.Producer] = new Bag<Product>();
+            }
+
+            this.byProducer[product.Producer].Add(product);
+
+            if (!this.byName.ContainsKey(product.Name))
+            {
+                this.byName[product.Name] = new Bag<Product>();
+            }
+
+            this.byName[product.Name].Add(product);
+            this.byPrice.Add(product);
+        }
+
+        public int DeleteProductsByProducer(string producer)
+        {
+            if (!this.byProducer.ContainsKey(producer))
+            {
+                return 0;
+            }
+
+            IEnumerable<Product> productsToRemove = this.byProducer[producer];
+
+            int count = 0;
+            foreach (var product in productsToRemove)
+            {
+                string name = product.Name;
+                this.byName[name].Remove(product);
+                this.byPrice.Remove(product);
+                
+                count++;
+            }
+
+            this.byProducer.Remove(producer);
+            return count;
+        }
+
+        public int DeleteProductsByProducerAndName(string productName, string producer)
+        {
+            if (!this.byProducer.ContainsKey(producer))
+            {
+                return 0;
+            }
+
+            IEnumerable<Product> productsToRemove = this.byProducer[producer]
+                .Where(x => x.Name == productName)
+                .ToList();
+
+            int count = 0;
+            foreach (var product in productsToRemove)
+            {
+                string name = product.Name;
+                this.byName[name].Remove(product);
+                this.byPrice.Remove(product);
+                count++;
+            }
+
+            return count;
+        }
+
+        public IEnumerable<Product> FindProductsByName(string name)
+        {
+            if (!this.byName.ContainsKey(name))
+            {
+                return Enumerable.Empty<Product>();
+            }
+
+            return this.byName[name].OrderBy(x => x);
+        }
+        public IEnumerable<Product> FindProductsByProducer(string producer)
+        {
+            if (!this.byProducer.ContainsKey(producer))
+            {
+                return Enumerable.Empty<Product>();
+            }
+
+            return this.byProducer[producer].OrderBy(x => x);
+        }
+        public IEnumerable<Product> FindProductsByPriceRange(double low, double high)
+        {
+            return this.byPrice
+                .Range(new Product(string.Empty, low, string.Empty),
+                    true,
+                    new Product(string.Empty, high, string.Empty),
+                    true)
+                .OrderBy(x => x); 
+        }
+    }
+}
---
+++ 18. Data structures - Dec 2018/13. Combining Data Structures/Shopping Center/Shopping Center/StartUp.cs
@@ -0,0 +1,102 @@
+﻿using Shopping_Center;
+using System.Text;
+
+StringBuilder builder = new StringBuilder();
+ShoppingCenter shoppingCenter = new ShoppingCenter();
+
+int commandNumber = int.Parse(Console.ReadLine()!);
+
+for (int i = 0; i < commandNumber; i++)
+{
+    string line = Console.ReadLine()!;
+
+    int firstSpace = line.IndexOf(' ');
+
+    string command = line.Substring(0, firstSpace);
+    string[] argms = line.Substring(firstSpace + 1).Split(';');
+
+    switch (command)
+    {
+        case "AddProduct":
+            string name = argms[0];
+            double price = double.Parse(argms[1]);
+            string producer = argms[2];
+
+            Product product = new Product(name, price, producer);
+            shoppingCenter.Add(product);
+            builder.AppendLine("Product added");
+            break;
+        case "DeleteProducts":
+            if (argms.Length == 1)
+            {
+                int count = shoppingCenter.DeleteProductsByProducer(argms[0]);
+                if (count == 0)
+                {
+                    builder.AppendLine("No products found");
+                }
+                else
+                {
+                    builder.AppendLine($"{count} products deleted");
+                }
+            }
+            else
+            {
+                int count = shoppingCenter.DeleteProductsByProducerAndName(argms[0], argms[1]);
+                if (count == 0)
+                {
+                    builder.AppendLine("No products found");
+                }
+                else
+                {
+                    builder.AppendLine($"{count} products deleted");
+                }
+            }
+            break;
+        case "FindProductsByName":
+            List<Product> resultByName = shoppingCenter.FindProductsByName(argms[0]).ToList();
+
+            if (resultByName.Count != 0)
+            {
+                resultByName.ForEach(x => builder.AppendLine(x.ToString()));
+            }
+            else
+            {
+                builder.AppendLine("No products found");
+            }
+            break;
+        case "FindProductsByProducer":
+            List<Product> resultByProducer = shoppingCenter.FindProductsByProducer(argms[0]).ToList();
+
+            if (resultByProducer.Count != 0)
+            {
+                resultByProducer.ForEach(x => builder.AppendLine(x.ToString()));
+            }
+            else
+            {
+                builder.AppendLine("No products found");
+            }
+            break;
+        case "FindProductsByPriceRange":
+            List<Product> resultByPriceRange = shoppingCenter.FindProductsByPriceRange(
+                    double.Parse(argms[0]),
+                    double.Parse(argms[1]))
+                .OrderBy(x => x)
+                .ToList();
+
+            if (resultByPriceRange.Count != 0)
+            {
+                resultByPriceRange.ForEach(x => builder.AppendLine(x.ToString()));
+            }
+            else
+            {
+                builder.AppendLine("No products found");
+            }
+            break;
+        default:
+            break;
+    }
+}
+
+Console.WriteLine(builder
+    .ToString()
+    .Trim());
